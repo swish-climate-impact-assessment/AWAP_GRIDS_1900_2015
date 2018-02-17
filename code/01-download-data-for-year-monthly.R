94b1b7527eaaff7a0b43f7115c46b93964f15553
ivanhanigan <ivan.hanigan@gmail.com>
2018-02-17 14:42:56 +1100

    add the monthly codes to 1900-2015

---
+++ code/01-download-data-for-year-monthly.R
@@ -0,0 +1,14 @@
+projdir <- "~/ResearchData/AWAP_GRIDS/AWAP_GRIDS_1900_2015"
+projdir
+setwd(projdir)
+
+dir()
+dir.create("data_provided_monthly")
+setwd("data_provided_monthly")
+# get weather data, beware that each grid is a couple of megabytes
+for(yy in 1901:1905){
+    ## yy <- 1900
+    load_monthly(start = sprintf('%s-01-01', yy),end = sprintf('%s-12-1', yy))
+}
+
+setwd(projdir)
